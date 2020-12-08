1685c986658ca651ad2f80467523c9022afa0c4e
jens <jens_johansen1@hotmail.com>
2020-12-08 17:07:12 +0100

    48kHz interrupt works

--- dubsiren.h
+++ dubsiren.h
@@ -1,3 +1,8 @@
+#ifndef __DUBSIREN_H
+#define __DUBSIREN_H
+
+#define PIN_LED         32
+#define PIN_PWM         5
 #define PIN_POT         A1
 #define PIN_I2S_BCLK    1
 #define PIN_I2S_WCLK    2
@@ -22,7 +27,7 @@ enum waveform_t {SQUARE, SAW, TRIANGLE, SINE};
 typedef uint16_t qs15_t; // signed fixed point [-1 - 1)
 typedef uint16_t qu16_t; // unsigned fixed point [0 - 1)
 
-inline qs15_t qu16_to_qs15 (float x) {return (qs15_t) (x >> 1);}
+inline qs15_t qu16_to_qs15 (qu16_t x) {return (qs15_t) (x >> 1);}
 inline qs15_t float_to_qs15 (float x) {return (qs15_t) (x * 0x8000);}
 inline qu16_t float_to_qu16 (float x) {return (qu16_t) (x * 0x10000);}
 inline qs15_t qs15_invert (qs15_t x) {return (x ^ 0xfff) + 1;}
@@ -44,3 +49,5 @@ inline uint16_t mul_qu16_uint16 (qu16_t x , uint16_t y) {
     // multiply and normalize
     return (uint16_t) (((uint32_t) x * y) >> 16);
 }
+
+#endif
--- dubsiren.ino
+++ dubsiren.ino
@@ -6,46 +6,68 @@
 
 volatile waveform_t osc_waveform = SINE;
 volatile int osc_period          = SAMPLE_RATE / 440; // in samples per period
-volatile qu16_t osc_resolution   = float_to_qu16_t(1 / osc_period); // sample resolution normalized to number of periods
+volatile qu16_t osc_resolution   = float_to_qu16(1 / osc_period); // sample resolution normalized to number of periods
 volatile qu16_t osc_phase        = 0; // always positive [0 - 1]
 volatile int osc_amplitude       = OSC_AMP_MAX / 10;
 
 volatile waveform_t lfo_waveform = SINE;
 volatile int lfo_period          = SAMPLE_RATE;
-volatile qu16_t lfo_resolution   = float_to_qu16_t(1 / lfo_period);
+volatile qu16_t lfo_resolution   = float_to_qu16(1 / lfo_period);
 volatile qu16_t lfo_phase        = 0;
 volatile int lfo_amplitude       = 1;
 
+bool led_state                   = true;
+volatile bool pwm_state          = true;
+
 
 void setup () {
-    pinMode(PIN_I2S_BCLK, OUTPUT);
-    pinMode(PIN_I2S_WCLK, OUTPUT);
-    pinMode(PIN_I2S_DATA, OUTPUT);
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_PWM, OUTPUT);
+    // pinMode(PIN_I2S_BCLK, OUTPUT);
+    // pinMode(PIN_I2S_WCLK, OUTPUT);
+    // pinMode(PIN_I2S_DATA, OUTPUT);
 
     Serial.begin(115200);
+    //while (!Serial); // wait for a serial connection (terminal)
 
-    if (!I2S.begin(I2S_PHILIPS_MODE, SAMPLE_RATE, 16)) {
-        Serial.println("Failed to initialize I2S");
-        while (1); // do nothing
-    }
+    // if (!I2S.begin(I2S_PHILIPS_MODE, SAMPLE_RATE, 16)) {
+    //     Serial.println("Failed to initialize I2S");
+    //     //while (1); // do nothing
+    // }
 
-    // enable APB bus clock for I2C and TC3 modules
-    PM_APBCMASK      = PM_APBCMASK_TC3 | PM_APBCMASK_I2S;
+    // PORT->Group[0].PMUX[9].bit.PMUXO     = 0x4;  // mux function E (TC/TCC) to PA19 (WO1)
+    // PORT->Group[0].PINCFG[19].bit.PMUXEN = 0x1; // PA19 Mux ON
 
-    // set TC3 to generate 48kHz interrupts
-    REG_TC3_CTRLC    = TC_CTRLC_CPTEN0; // enable capture on channel 0 (necessary in MFRQ mode?)
-    REG_TC3_INTENSET = TC_CTRLC_MC0;    // enable capture channel 0 interrupt
-    REG_TC3_CC0      = 1000;            // set period to 1000 cycles for a 48kHz frequency
-    REG_TC3_CTRLA    = TC_CTRLA_WAVEGEN_MFRQ | TC_CTRLA_ENABLE;  // 16-bit mode, no prescaler, MFRQ mode, enable timer
+    GCLK->CLKCTRL.bit.ID = 0x1B;             // Clock selection (TC3)
+    GCLK->CLKCTRL.bit.GEN = 0x0;             // Clock generator 0
+    GCLK->CLKCTRL.bit.CLKEN = 0x1;           // Enable
 
-    // Enable TC3 interrupt in the NVIC
-    NVIC_ISER        = 1 << IRQn[TC3_IRQn];
+    PM->APBCMASK.bit.TC3_ = 0x1;             // enable APB bus clock for TC3 module
+
+    TC3->COUNT16.INTENSET.bit.MC0 = 1;       // enable capture channel 0 interrupt
+
+    TC3->COUNT16.CTRLA.bit.ENABLE = 0;       // disable TC3
+    TC3->COUNT16.CTRLA.bit.PRESCSYNC = 0x1;  // 48MHz
+    TC3->COUNT16.CTRLA.bit.MODE = 0x00;      // 16 Bit Timer
+    TC3->COUNT16.CTRLA.bit.WAVEGEN = 0x3;    // MPWM
+    TC3->COUNT16.CTRLA.bit.PRESCALER = 0x00; // 1
+    TC3->COUNT16.CC[0].bit.CC = 1000;        // capture every 1000 cycle -> 48 kHz
+
+    NVIC_ClearPendingIRQ(TC3_IRQn);
+    NVIC_SetPriority(TC3_IRQn, 1);
+    NVIC_EnableIRQ(TC3_IRQn);
+
+    TC3->COUNT16.CTRLA.bit.ENABLE = 1;       // enable TC3
+
+    Serial.println("Initialization completed");
 }
 
 
 void loop () {
-    // read frequency from pot
-    delay(1)
+    Serial.println("tik");
+    digitalWrite(PIN_LED, led_state);
+    led_state = !led_state;
+    delay(1000);
 }
 
 
@@ -87,20 +109,26 @@ qs15_t inline getAmplitude(waveform_t waveform, qu16_t phase) {
 }
 
 
-
-
 void TC3_Handler() {
 
-    uint16_t sample;
+    // clear interrupt flag
+    TC3->COUNT16.INTFLAG.bit.MC0 = 1;
 
-    // update oscillator and lfo phase. these overflow naturally
-    osc_phase += osc_resolution;
-    lfo_phase += lfo_resolution;
+    digitalWrite(PIN_PWM, pwm_state);
+    pwm_state = !pwm_state;
 
-    // get oscillator value
-    sample = OSC_BIAS + osc_amplitude * getAmplitude(osc_waveform, osc_phase);
 
-    // write sample to DAC for both channels
-    I2S.write(sample);
-    I2S.write(sample);
+
+    // uint16_t sample;
+    //
+    // // update oscillator and lfo phase. these overflow naturally
+    // osc_phase += osc_resolution;
+    // lfo_phase += lfo_resolution;
+    //
+    // // get oscillator value
+    // sample = OSC_BIAS + osc_amplitude * getAmplitude(osc_waveform, osc_phase);
+    //
+    // // write sample to DAC for both channels
+    // I2S.write(sample);
+    // I2S.write(sample);
 }
